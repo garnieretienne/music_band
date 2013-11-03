292b522ce2f2bb9881ea46d2db7ea976168e0d31
Etienne Garnier <garnier.etienne@gmail.com>
2013-11-03 14:32:22 +0100

    manage publications under the 'admin' namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 MusicBand::Application.routes.draw do
-  resources :publications
+  
+  namespace :admin do
+    resources :publications
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
