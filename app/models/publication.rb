a2409e38e519262a559f9bf4c3a77741611053b9
Etienne Garnier <garnier.etienne@gmail.com>
2013-11-07 16:35:24 +0100

    add markdown extention to convert youtube link to embedded youtube player

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -18,7 +18,7 @@ class Publication < ActiveRecord::Base
   # Markdown extention: replace every youtube links with embedded youtube video
   def markdown_extention_youtube_links(html)
     html = html.gsub /<a\shref="http:\/\/www\.youtube\.com\/watch\?v=(.*).*".*>.*<\/a>/o, 
-      "<iframe class=\"youtube-player\" width=\"640\" height=\"390\" src=\"http://www.youtube.com/embed/\\1\"></iframe>"
+      "<p><iframe class=\"youtube-player\" width=\"640\" height=\"390\" src=\"http://www.youtube.com/embed/\\1\"></iframe></p>"
     return html
   end
 end
